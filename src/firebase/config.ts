6205633d1b214df669e676262879c4b4cb8f0420
Muhammad Zeeshan <ZeeshanZahid663@gmail.com>
2026-02-01 17:20:48 +0500

    src/firebase/config.ts FIREBASE_CONFIG

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCigrz7AkS8SVKJ9XHh2_QKLINMNWX2RA8",
-  authDomain: "studio-2539723907-d7516.firebaseapp.com",
-  projectId: "studio-2539723907-d7516",
-  storageBucket: "studio-2539723907-d7516.firebasestorage.app",
-  messagingSenderId: "811456090413",
-  appId: "1:811456090413:web:3e5f95c8f4f0c8b42b43a4",
-  measurementId: "G-KV3E67NZG7"
-};
+export const firebaseConfig = process.env.FIREBASE_CONFIG;
